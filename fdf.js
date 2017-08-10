b3711cd8a1567f71413d9f8abc1170887980cc52
sbresin <sebastian.bresin@gmail.com>
2017-08-10 11:36:13 +0200

    cleanup, lint, alpha nested field support

--- fdf.js
+++ fdf.js
@@ -1,74 +1,85 @@
-var fs = require('fs');
-var _ = require('lodash');
-var dateFormat = require('dateformat');
+// var fs = require('fs');
+var _ = require('lodash')
+const dateFormat = require('dateformat')
 
 // only this sequence in FDF header requires char codes
-var headerChars = new Buffer(
-    (String.fromCharCode(226)) +
-    (String.fromCharCode(227)) +
-    (String.fromCharCode(207)) +
-    (String.fromCharCode(211)) +
-    "\n"
-);
-
-var header = Buffer.concat([
-    new Buffer("%FDF-1.2\n"),
-    headerChars,
-    new Buffer(
-        "1 0 obj \n" +
-        "<<\n" +
-        "/FDF \n" +
-        "<<\n" +
-        "/Fields [\n"
-    )
-]);
-
-var footer = new Buffer(
-    "]\n" +
-    ">>\n" +
-    ">>\n" +
-    "endobj \n" +
-    "trailer\n" +
-    "\n" +
-    "<<\n" +
-    "/Root 1 0 R\n" +
-    ">>\n" +
-    "%%EOF\n"
+var headerChars = Buffer.from(
+  (String.fromCharCode(226)) +
+  (String.fromCharCode(227)) +
+  (String.fromCharCode(207)) +
+  (String.fromCharCode(211)) +
+  '\n'
 )
 
-var escapeString = function escapeString(value) {
-    // format dates
-    var out;
-    if(value instanceof Date){
-        out = dateFormat(value, "dd.mm.yyyy");
-    } else {
-        out = value.toString();
-    }
-    out = out.replace(/\\/g, "\\\\");
-    out = out.replace(/\(/g, "\\(");
-    out = out.replace(/\)/g, "\\)");
-    out = out.toString("utf8");
-    return out;
+var header = Buffer.concat([
+  Buffer.from('%FDF-1.2\n'),
+  headerChars,
+  Buffer.from(
+    '1 0 obj \n' +
+    '<<\n' +
+    '/FDF \n' +
+    '<<\n' +
+    '/Fields [\n'
+  )
+])
+
+var footer = Buffer.from(
+  ']\n' +
+  '>>\n' +
+  '>>\n' +
+  'endobj \n' +
+  'trailer\n' +
+  '\n' +
+  '<<\n' +
+  '/Root 1 0 R\n' +
+  '>>\n' +
+  '%%EOF\n'
+)
+
+var escapeString = function escapeString (value) {
+  // format dates
+  var out
+  if (value instanceof Date) {
+    out = dateFormat(value, 'dd.mm.yyyy')
+  } else {
+    out = value.toString()
+  }
+  out = out.replace(/\\/g, '\\\\')
+  out = out.replace(/\(/g, '\\(')
+  out = out.replace(/\)/g, '\\)')
+  out = out.toString('utf8')
+  return out
+}
+
+var concatBody = function concatBody (body, value, name) {
+  body = Buffer.concat([
+    body,
+    Buffer.from(
+      '<<\n' +
+      '/T (' +
+      escapeString(name) +
+      ')\n' +
+      '/V (' +
+      escapeString(value) +
+      ')\n' +
+      '>>\n'
+    )
+  ])
+  return body
 }
 
 exports.createFdf = function (data) {
-    var body = new Buffer([]);
+  var body = Buffer.from([])
 
-    _.mapKeys(data, function (value, name) {
-        body = Buffer.concat([
-            body,
-            new Buffer(
-                "<<\n" +
-                "/T (" +
-                escapeString(name) +
-                ")\n" +
-                "/V (" +
-                escapeString(value) +
-                ")\n" +
-                ">>\n"
-            )
-        ]);
-    });
+  _.mapKeys(data, function (value, name) {
+    if (_.isObject(value)) {
+      _.mapKeys(value, function (value2, name2) {
+        body = concatBody(body, value2, name + '.' + name2)
+      })
+    } else {
+      body = concatBody(body, value, name)
+    }
+  })
 
-    return Buffer.concat([header, body, footer]);
+  return Buffer.concat([header, body, footer])
 }
--- index.js
+++ index.js
@@ -1,187 +1,176 @@
-var spawn = require('child_process').spawn,
-    fdf = require("./fdf.js"),
-    _ = require('lodash'),
-    iconv = require('iconv-lite');
-    fs = require('fs');
+var spawn = require('child_process').spawn
+var fdf = require('./fdf.js')
+var _ = require('lodash')
+var iconv = require('iconv-lite')
+var fs = require('fs')
 
 var pdffiller = {
 
-    mapForm2PDF: function (formFields, convMap) {
-        var tmpFDFData = this.convFieldJson2FDF(formFields);
-        tmpFDFData = _.mapKeys(tmpFDFData, function (value, key) {
-            try {
-                convMap[key];
-            } catch (err) {
+  /*
+  mapForm2PDF: function (formFields, convMap) {
+    var tmpFDFData = this.convFieldJson2FDF(formFields)
+    tmpFDFData = _.mapKeys(tmpFDFData, function (value, key) {
+      try {
+        convMap[key]
+      } catch (err) {
+        return key
+      }
+      return convMap[key]
+    })
 
-                return key;
-            }
-            return convMap[key];
-        });
+    return tmpFDFData
+  },
+ */
 
-        return tmpFDFData;
-    },
+  convFieldJson2FDF: function (fieldJson) {
+    var _keys = _.map(fieldJson, 'title')
+    var _values = _.map(fieldJson, 'fieldValue')
 
-    convFieldJson2FDF: function (fieldJson) {
-        var _keys = _.map(fieldJson, 'title'),
-            _values = _.map(fieldJson, 'fieldValue');
+    _values = _.map(_values, function (val) {
+      if (val === true) {
+        return 'Yes'
+      } else if (val === false) {
+        return 'Off'
+      }
+      return val
+    })
 
-        _values = _.map(_values, function (val) {
-            if (val === true) {
-                return 'Yes';
-            } else if (val === false) {
-                return 'Off';
-            }
-            return val;
-        });
+    var jsonObj = _.zipObject(_keys, _values)
 
-        var jsonObj = _.zipObject(_keys, _values);
+    return jsonObj
+  },
 
-        return jsonObj;
-    },
+  generateFieldJson: function (sourceFile, nameRegex) {
+    var regName = /FieldName: ([^\n]*)/
+    var regType = /FieldType: ([A-Za-z\t .]+)/
+    var regFlags = /FieldFlags: ([0-9\t .]+)/
+    var fieldArray = []
+    var currField = {}
 
-    generateFieldJson: function (sourceFile, nameRegex) {
-        var regName = /FieldName: ([^\n]*)/,
-            regType = /FieldType: ([A-Za-z\t .]+)/,
-            regFlags = /FieldFlags: ([0-9\t .]+)/,
-            fieldArray = [],
-            currField = {};
+    if (nameRegex !== null && (typeof nameRegex) === 'object') regName = nameRegex
 
-        if (nameRegex !== null && (typeof nameRegex) == 'object') regName = nameRegex;
+    return new Promise(function (resolve, reject) {
+      var childProcess = spawn('pdftk', [sourceFile, 'dump_data_fields_utf8'])
+      var output = ''
 
-        return new Promise(function (resolve, reject) {
-            var childProcess = spawn("pdftk", [sourceFile, "dump_data_fields_utf8"]);
-            var output = '';
+      childProcess.on('error', function (err) {
+        console.log('pdftk exec error: ' + err)
+        reject(err)
+      })
 
-            childProcess.on('error', function (err) {
-                console.log('pdftk exec error: ' + err);
-                reject(err);
-            });
+      childProcess.stdout.on('data', function (data) {
+        output += data
+      })
 
-            childProcess.stdout.on('data', function (data) {
-                output += data;
-            });
+      childProcess.stdout.on('end', function () {
+        var fields = output.split('---').slice(1)
 
-            childProcess.stdout.on('end', function () {
+        fields.forEach(function (field) {
+          currField = {}
+          currField['title'] = field.match(regName)[1].trim() || ''
 
-                fields = output.split("---").slice(1);
+          if (field.match(regType)) {
+            currField['fieldType'] = field.match(regType)[1].trim() || ''
+          } else {
+            currField['fieldType'] = ''
+          }
 
-                fields.forEach(function (field) {
-                    currField = {};
-                    currField['title'] = field.match(regName)[1].trim() || '';
+          if (field.match(regFlags)) {
+            currField['fieldFlags'] = field.match(regFlags)[1].trim() || ''
+          } else {
+            currField['fieldFlags'] = ''
+          }
 
-                    if (field.match(regType)) {
-                        currField['fieldType'] = field.match(regType)[1].trim() || '';
-                    } else {
-                        currField['fieldType'] = '';
-                    }
+          currField['fieldValue'] = ''
+          fieldArray.push(currField)
+        })
 
-                    if (field.match(regFlags)) {
-                        currField['fieldFlags'] = field.match(regFlags)[1].trim() || '';
-                    } else {
-                        currField['fieldFlags'] = '';
-                    }
+        resolve(fieldArray)
+      })
+    })
+  },
 
-                    currField['fieldValue'] = '';
-                    fieldArray.push(currField);
-                });
+  generateFDFTemplate: function (sourceFile, nameRegex) {
+    return new Promise(function (resolve, reject) {
+      this.generateFieldJson(sourceFile, nameRegex).then(function (formFields) {
+        var _keys = _.map(formFields, 'title')
+        var _values = _.map(formFields, 'fieldValue')
+        var jsonObj = _.zipObject(_keys, _values)
 
-                resolve(fieldArray);
-            });
+        resolve(jsonObj)
+      }).catch(function (err) {
+        reject(err)
+      })
+    }.bind(this))
+  },
 
-        });
-    },
+  fillFormWithOptions: function (sourceFile, fieldValues, shouldFlatten) {
+    var promised = new Promise(function (resolve, reject) {
+      // Generate the data from the field values.
+      // var randomSequence = Math.random().toString(36).substring(7)
+      // var currentTime = new Date().getTime()
+      var FDFinput = fdf.createFdf(fieldValues)
+      var FDFinputString = iconv.decode(FDFinput, 'utf-8')
+      FDFinput = iconv.encode(FDFinputString, 'ISO-8859-1')
 
-    generateFDFTemplate: function (sourceFile, nameRegex) {
-        return new Promise(function (resolve, reject) {
+      var args = [sourceFile, 'fill_form', '-', 'output', '-']
+      if (shouldFlatten) {
+        args.push('flatten')
+      }
 
-            this.generateFieldJson(sourceFile, nameRegex).then(function (_form_fields) {
+      var childProcess = spawn('pdftk', args)
 
-                var _keys = _.map(_form_fields, 'title'),
-                    _values = _.map(_form_fields, 'fieldValue'),
-                    jsonObj = _.zipObject(_keys, _values);
+      childProcess.stderr.on('data', function (err) {
+        console.log('pdftk exec error: ' + err)
+        reject(err)
+      })
 
-                resolve(jsonObj);
+      function sendData (data) {
+        childProcess.stdout.pause()
+        childProcess.stdout.unshift(data)
+        resolve(childProcess.stdout)
+        childProcess.stdout.removeListener('data', sendData)
+      };
 
-            }).catch(function (err) {
+      childProcess.stdout.on('data', sendData)
 
-                reject(err);
+      // now pipe FDF to pdftk
+      childProcess.stdin.write(FDFinput)
+      childProcess.stdin.end()
+    })
 
-            });
-        }.bind(this));
-    },
+    // bind convenience method toFile for chaining
+    promised.toFile = toFile.bind(null, promised)
+    return promised
+  },
 
-    fillFormWithOptions: function (sourceFile, fieldValues, shouldFlatten) {
+  fillFormWithFlatten: function (sourceFile, fieldValues, shouldFlatten) {
+    return this.fillFormWithOptions(sourceFile, fieldValues, shouldFlatten)
+  },
 
-        var promised = new Promise(function (resolve, reject) {
+  fillForm: function (sourceFile, fieldValues) {
+    return this.fillFormWithFlatten(sourceFile, fieldValues, true)
+  }
 
-            //Generate the data from the field values.
-            var randomSequence = Math.random().toString(36).substring(7);
-            var currentTime = new Date().getTime();
-            var FDFinput = fdf.createFdf(fieldValues);
-            var FDFinputString = iconv.decode(FDFinput, "utf-8");
-            FDFinput = iconv.encode(FDFinputString, "ISO-8859-1");
-
-            var args = [sourceFile, "fill_form", '-', "output", '-'];
-            if (shouldFlatten) {
-                args.push("flatten");
-            }
-
-            var childProcess = spawn("pdftk", args);
-
-            childProcess.stderr.on('data', function (err) {
-                console.log('pdftk exec error: ' + err);
-                reject(err);
-            });
-
-	        function sendData (data) {
-                childProcess.stdout.pause();
-                childProcess.stdout.unshift(data);
-                resolve(childProcess.stdout);
-                childProcess.stdout.removeListener('data', sendData);
-            };
-
-            childProcess.stdout.on('data', sendData);
-
-            // now pipe FDF to pdftk
-            childProcess.stdin.write(FDFinput);
-            childProcess.stdin.end();
-
-        });
-
-        // bind convenience method toFile for chaining
-        promised.toFile = toFile.bind(null, promised);
-        return promised;
-    },
-
-    fillFormWithFlatten: function (sourceFile, fieldValues, shouldFlatten) {
-        return this.fillFormWithOptions(sourceFile, fieldValues, shouldFlatten);
-    },
-
-    fillForm: function (sourceFile, fieldValues) {
-        return this.fillFormWithFlatten(sourceFile, fieldValues, true);
-    },
-
-};
+}
 
 /**
  * convenience chainable method for writing to a file (see examples)
  **/
 function toFile (promised, path) {
-    return new Promise(function (resolve, reject) {
-        promised.then(function(outputStream) {
+  return new Promise(function (resolve, reject) {
+    promised.then(function (outputStream) {
+      var output = fs.createWriteStream(path)
 
-            var output = fs.createWriteStream(path);
-
-            outputStream.pipe(output);
-            outputStream.on('close', function() {
-                output.end();
-                resolve();
-            });
-
-        }).catch(function (error) {
-            reject(error);
-        });
-    });
+      outputStream.pipe(output)
+      outputStream.on('close', function () {
+        output.end()
+        resolve()
+      })
+    }).catch(function (error) {
+      reject(error)
+    })
+  })
 }
 
-module.exports = pdffiller;
+module.exports = pdffiller
